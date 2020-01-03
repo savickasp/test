29ef98a3ab4c415df80ffa8957054346244c8a04
savickasp <savickas.p@gmail.com>
2020-01-03 13:05:55 +0200

    session edit

--- index.php
+++ index.php
@@ -2,17 +2,7 @@
 
 session_start();
 
-function generateID($length) {
-    $ret = '';
-
-    for ($i = 0; $i < $length; $i++){
-        $ret .= rand(0, 9);
-    }
-
-    return $ret;
-}
-$id = $_SESSION['id'] ?? generateID(16);
-$_SESSION['id'] = $id;
+$id = $_COOKIE['PHPSESSID'];
 
 $count = ($_SESSION['visits'] ?? 0) + 1;
 $_SESSION['visits'] = $count;
