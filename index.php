23fa6a798d35d55e7d3f5b89c664af75f1fa6854
savickasp <savickas.p@gmail.com>
2020-01-03 10:34:24 +0200

    edit

--- index.php
+++ index.php
@@ -13,3 +13,16 @@ if(!isset($_COOKIE['userID'])) {
 
 $count = ($_COOKIE['visits'] ?? 0) + 1;
 setcookie('visits', $count, time() + 3600);
+
+$h1 = $_COOKIE['userID'];
+$h2 = $count;
+?>
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+    <body>
+        <h1><?php print $h1; ?></h1>
+        <h2><?php print $h2; ?></h2>
+    </body>
+</html>
